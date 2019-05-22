bb5e148c62c9879a88e6d81b751a61107ee67df3
MrShawn <thunderboy288@gmail.com>
2019-05-22 01:10:54 -0400

    Added configurable coordinates for respawning

--- src/main/java/dev/mrshawn/wdwgan/listeners/RespawnListener.java
+++ src/main/java/dev/mrshawn/wdwgan/listeners/RespawnListener.java
@@ -2,6 +2,8 @@ package dev.mrshawn.wdwgan.listeners;
 
 import dev.mrshawn.wdwgan.WDWGAN;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,8 +16,15 @@ public class RespawnListener implements Listener {
 	@EventHandler
 	public void onRespawn(PlayerRespawnEvent e) {
 		Player player = e.getPlayer();
-		if (e.isBedSpawn()) {
+		if (e.isBedSpawn() && !instance.getConfig().getBoolean("coordinates.enabled")) {
 			e.setRespawnLocation(Bukkit.getServer().getWorld(instance.getConfig().getString("respawn-world")).getSpawnLocation());
+		} else if (e.isBedSpawn() && instance.getConfig().getBoolean("coordinates.enabled")) {
+			int x = instance.getConfig().getInt("coordinates.x");
+			int y = instance.getConfig().getInt("coordinates.y");
+			int z = instance.getConfig().getInt("coordinates.z");
+			World world = Bukkit.getServer().getWorld(instance.getConfig().getString("respawn-world"));
+			Location location = new Location(world, x, y, z);
+			e.setRespawnLocation(location);
 		}
 	}
 
