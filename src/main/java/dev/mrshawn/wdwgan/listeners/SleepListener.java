12937bfdf71cf674791f5c490f3e093ef1eb8870
Shawn11564 <thunderboy288@gmail.com>
2019-05-26 10:23:23 -0400

    Trans-World bed travel... trippy

--- src/main/java/dev/mrshawn/wdwgan/listeners/SleepListener.java
+++ src/main/java/dev/mrshawn/wdwgan/listeners/SleepListener.java
@@ -8,12 +8,20 @@ import org.bukkit.event.player.PlayerBedEnterEvent;
 
 public class SleepListener implements Listener {
 
+	private static WDWGAN instance = WDWGAN.getInstance();
+	
 	@EventHandler
 	public void onSleep(PlayerBedEnterEvent e) {
 		Player player = e.getPlayer();
 		Block bed = e.getBed();
-		if (player.getWorld().getTime() >= 25000) {
-
+		Location bedLoc = bed.getLocation();
+		World original = player.getWorld();
+		World dreamworld = new World(instance.getConfig.getString("dream-world"));
+		if (!player.getWorld.equals(dreamworld)) {
+			e.setCanceled(true);
+			player.setLocation(new Location(dreamworld, 0, 100, 0);
+		} else if (player.getWorld.equals(dreamworld)) {
+			player.setLocation(new Location(original, bedLoc.getX(), bedLoc.getY(), getLoc.getZ()));
 		}
 	}
 
